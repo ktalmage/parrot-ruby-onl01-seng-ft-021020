2b43d5aede199ddedc52421e8a6f1926708028d9
Kyle Talmage <ktalmage85@gmail.com>
2020-02-10 17:50:49 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
+def parrot(string = "Squawk!")
+  puts "This parrot says #{string}"
+  return string
+end
+
+parrot 
+
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
